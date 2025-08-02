bb0f869ea4ec25f85bff9ff2427e591621b4c35c
Nick Wu <wu2264@purdue.edu>
2025-08-02 15:43:44 -0400

    Initial commit

---
+++ read.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+
+# img = cv.imread('Bible Page Data/proverbs_12.jpeg')
+# cv.imshow('Proverbs 12', img)
+
+# Reading Videos
+capture = cv.VideoCapture(1)
+
+if not capture.isOpened():
+    print("Cannot open camera")
+    exit()
+
+while True:
+    isTrue, frame = capture.read()
+    cv.imshow('Walking', frame)
+
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
